0f89aa109414b88752c1901ecec38fa963a3c242
Alfardil Alam <fardilfayed@gmail.com>
2025-07-08 23:33:45 -0400

    fixed eslint warnings

--- frontend/components/ui/ace/macbook-scroll.tsx
+++ frontend/components/ui/ace/macbook-scroll.tsx
@@ -23,6 +23,7 @@ import { IconWorld } from "@tabler/icons-react";
 import { IconCommand } from "@tabler/icons-react";
 import { IconCaretLeftFilled } from "@tabler/icons-react";
 import { IconCaretDownFilled } from "@tabler/icons-react";
+import Image from "next/image";
 
 export const MacbookScroll = ({
   src,
@@ -164,9 +165,12 @@ export const Lid = ({
         className="absolute inset-0 h-96 w-[32rem] rounded-2xl bg-[#010101] p-2"
       >
         <div className="absolute inset-0 rounded-lg bg-[#272729]" />
-        <img
+        <Image
           src={src as string}
           alt="gitty logo"
+          width={2000}
+          height={2000}
+          quality={100}
           className="absolute inset-0 h-full w-full rounded-lg object-cover object-left-top"
         />
       </motion.div>
